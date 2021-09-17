ec32b9105de81a3a5a3c9b3ad96154fc74d03a88
Mienard Lumaad <themepixels@users.noreply.github.com>
2021-09-17 21:55:04 +0800

    update styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -79,29 +79,41 @@ $(function(){
   // for demo only
   $('#navigationSkins a').on('click', function(e){
     e.preventDefault();
+
     var s = $(this).attr('data-skin');
+    localStorage.setItem('skin', s);
 
-    $(this).addClass('active').siblings().removeClass('active');
-
-    $('body').attr('class', function(i, c){
-      return c.replace(/(^|\s)skin-\S+/g, '');
-    });
-
-    $('body').addClass('skin-'+s);
+    location.reload();
   });
 
   $('#navigationStyles a').on('click', function(e){
     e.preventDefault();
+
     var s = $(this).attr('data-style');
+    localStorage.setItem('style', s);
 
-    $(this).addClass('active').siblings().removeClass('active');
-
-    $('.nav-sidebar').attr('class', function(i, c){
-      return c.replace(/(^|\s)style-\S+/g, '');
-    });
-
-    if(s !== 'base') {
-      $('.nav-sidebar').addClass('style-'+s);
-    }
+    location.reload();
   });
-})
+
+  //on page load
+  var sk = localStorage.getItem('skin');
+  var st = localStorage.getItem('style');
+
+  // skin
+  $('body').attr('class', function(i, c){
+    return c.replace(/(^|\s)skin-\S+/g, '');
+  });
+
+  $('body').addClass('skin-'+sk);
+  $('#navigationSkins a[data-skin='+sk+']').addClass('active').siblings().removeClass('active');
+
+  //sidebar nav style
+  $('.nav-sidebar').attr('class', function(i, c){
+    return c.replace(/(^|\s)style-\S+/g, '');
+  });
+
+  $('.nav-sidebar').addClass('style-'+st, (st !== 'base'));
+  $('#navigationStyles a[data-style='+st+']').addClass('active').siblings().removeClass('active');
+
+
+});
